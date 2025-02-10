f2badb3df588de789339b8e09f7a226b238a232d
Nguyễn Thành Đạt <datntse180197@fpt.edu.vn>
2025-02-10 15:30:24 +0700

    init code

---
+++ commitlint.config.js
@@ -0,0 +1,26 @@
+module.exports = {
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'type-enum': [
+      2,
+      'always',
+      [
+        'feat',
+        'fix',
+        'docs',
+        'style',
+        'refactor',
+        'perf',
+        'test',
+        'chore',
+        'revert',
+        'build',
+      ],
+    ],
+    'type-case': [2, 'always', 'lowercase'],
+    'type-empty': [2, 'never'],
+    'scope-case': [2, 'always', 'lowercase'],
+    'subject-empty': [2, 'never'],
+    'subject-full-stop': [2, 'never', '.'],
+  },
+};
---
+++ src/app/page.tsx
@@ -0,0 +1,21 @@
+import { Button } from "antd";
+
+export default function Home() {
+  return (
+    <div>
+      hello world nextjs with hoidanit
+      <div>
+        <Button type="primary"> Add new</Button>
+        <div>
+          <Button type="primary"> Add new</Button>
+        </div>
+        <div>
+          <Button type="primary"> Add new</Button>
+        </div>
+        <div>
+          <Button type="primary"> Add new</Button>
+        </div>
+      </div>
+    </div>
+  );
+}
